d349b516208b66a564c82ce6ed6ab33d6cae320b
Arda Cetinkaya <ardacetinkaya@gmail.com>
2020-09-16 19:28:52 +0300

    MS SQL and Azure Cache resources

--- Infrastructure.cs
+++ Infrastructure.cs
@@ -1,15 +1,33 @@
-using System.Collections.Generic;
 using Pulumi;
 using Pulumi.Azure.Core;
+using Pulumi.Random;
 using Storage = Pulumi.Azure.Storage;
-using CosmosDB = Pulumi.Azure.CosmosDB;
 using KeyVault = Pulumi.Azure.KeyVault;
 using AppService = Pulumi.Azure.AppService;
-class Infrastructure : Stack
+using Redis = Pulumi.Azure.Redis;
+using SQL = Pulumi.Azure.Sql;
+using MSSQL = Pulumi.Azure.MSSql;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Infrastructure : Stack
 {
     public Infrastructure()
     {
+        var configuration = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("InfrastructureSettings.json", true, false)
+            .Build();
+
         var current = Output.Create(GetClientConfig.InvokeAsync());
+        var sqlAdminPassword = new RandomString("random", new RandomStringArgs
+        {
+            Length = 16,
+            OverrideSpecial = "=/:_@%",
+            Special = true,
+        });
 
         var resourceGroup = new ResourceGroup("product-resources", new ResourceGroupArgs
         {
@@ -51,6 +69,18 @@ class Infrastructure : Stack
             ResourceGroupName = resourceGroup.GetResourceName()
         }));
 
+        var cache = new Redis.Cache("product-cache-01", new Redis.CacheArgs
+        {
+            Name = "product-cache-01",
+            Location = resourceGroup.Location,
+            ResourceGroupName = resourceGroup.Name,
+            Capacity = 0,
+            Family = "C",
+            SkuName = "Basic",
+            EnableNonSslPort = false,
+            MinimumTlsVersion = "1.2"
+        });
+
         var apiAppService = new AppService.AppService("product-api-01", new AppService.AppServiceArgs
         {
             Name = "product-api-01",
@@ -62,7 +92,8 @@ class Infrastructure : Stack
             Identity = new AppService.Inputs.AppServiceIdentityArgs { Type = "SystemAssigned" },
             AppSettings = {
                 { "KEYVAULT_ADDRESS",appKeyVault.Apply(k => k.VaultUri)},
-                { "STORAGE_CONNECTIONSTRING",storageAccount.PrimaryConnectionString.Apply(con=>con)}
+                { "STORAGE_CONNECTIONSTRING",storageAccount.PrimaryConnectionString.Apply(con=>con)},
+                { "CACHE_CONNECTIONSTRING",cache.PrimaryConnectionString.Apply(con=>con)}
             }
 
         });
@@ -128,7 +159,7 @@ class Infrastructure : Stack
             }
         });
 
-        var keyVaultSecret = new KeyVault.Secret("sa-01-connection-string", new KeyVault.SecretArgs
+        var keyVaultSecret01 = new KeyVault.Secret("sa-01-connection-string", new KeyVault.SecretArgs
         {
             Name = "sa-01-connection-string",
             KeyVaultId = keyVault.Id,
@@ -136,12 +167,40 @@ class Infrastructure : Stack
             Value = storageAccount.PrimaryBlobConnectionString
         });
 
+        var keyVaultSecret02 = new KeyVault.Secret("sql-admin-pwd", new KeyVault.SecretArgs
+        {
+            Name = "sql-admin-pwd",
+            KeyVaultId = keyVault.Id,
+            ContentType = "text/plain",
+            Value = sqlAdminPassword.Result
+        });
+
+
+        var sqlServer = new SQL.SqlServer("product-sql-server-01", new SQL.SqlServerArgs
+        {
+            Name = "product-sql-server-01",
+            ResourceGroupName = resourceGroup.Name,
+            Location = resourceGroup.Location,
+            Version = "12.0",
+            AdministratorLogin = "4dm1n157r470r",
+            AdministratorLoginPassword = sqlAdminPassword.Result
+        });
+
+        var database = new MSSQL.Database("product-db-01", new MSSQL.DatabaseArgs
+        {
+            Name = "product-db-01",
+            ServerId = sqlServer.Id,
+            Collation = "SQL_Latin1_General_CP1_CI_AS",
+            MaxSizeGb = 2,
+            SkuName = "Basic"
+        });
 
 
         this.ConnectionString = storageAccount.PrimaryConnectionString;
         this.WebSiteAddress = webAppService.DefaultSiteHostname;
 
     }
+
 
     [Output]
     public Output<string> WebSiteAddress { get; set; }
